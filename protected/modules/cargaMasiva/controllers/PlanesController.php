74e041c8c66a88c11962c9d52dfdd4fd0313595a
Guillermo Enrique <gsanchez1687@gmail.com>
2024-02-02 09:41:30 -0500

    actualizacion del Framework a 1.1.20 14 de noviembre del 2023

--- protected/modules/cargaMasiva/controllers/PlanesController.php
+++ protected/modules/cargaMasiva/controllers/PlanesController.php
@@ -6,7 +6,7 @@ class PlanesController extends Controller
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
-	public $layout='//layouts/column2';
+	public $layout = '//layouts/column2';
 
 	/**
 	 * @return array action filters
@@ -27,20 +27,24 @@ class PlanesController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
+			array(
+				'allow',  // allow all users to perform 'index' and 'view' actions
+				'actions' => array('index', 'view'),
+				'users' => array('*'),
 			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update','carga'),
-				'users'=>array('@'),
+			array(
+				'allow', // allow authenticated user to perform 'create' and 'update' actions
+				'actions' => array('create', 'update', 'carga'),
+				'users' => array('@'),
 			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('@'),
+			array(
+				'allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions' => array('admin', 'delete'),
+				'users' => array('@'),
 			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
+			array(
+				'deny',  // deny all users
+				'users' => array('*'),
 			),
 		);
 	}
@@ -51,133 +55,155 @@ class PlanesController extends Controller
 	 */
 	public function actionView($id)
 	{
-		$this->render('view',array(
-			'model'=>$this->loadModel($id),
+		$this->render('view', array(
+			'model' => $this->loadModel($id),
 		));
 	}
-        
-        public function actionCarga(){
-          
-        if (Yii::app()->authRBAC->checkAccess('planes_carga')):    
-            try {            
-              
-                $model=new Planes;
 
-                 if(isset($_FILES['Planes'])){
-                       
-                              $model->attributes=$_POST['Planes'];
-                              $transaction=$model->dbConnection->beginTransaction();
+	public function actionCarga()
+	{
 
-                               $fname = $_FILES['Planes']['name']; 
-                               $chk_ext = explode(".",$fname['file']);
-                               if(strtolower(end($chk_ext)) == "csv"):
-                                   $filename = $_FILES['Planes']['tmp_name'];
-                                   $handle = fopen($filename['file'], "r");                            
-                                     while ($data = fgetcsv($handle,500,";")):                                          
-                                         $modelPlanes = new Planes;                                 
-                                         $modelPlanes->nombre = $data[0];
-                                         $modelPlanes->valor = $data[1];                                      
-                                         $modelPlanes->operador_id = $_POST['Planes']['operador_id'];                                        
-                                         $modelPlanes->fecha_vigencia = $_POST['Planes']['fecha_vigencia'];     
-                                         $modelPlanes->fecha_vencimiento = $_POST['Planes']['fecha_vencimiento'];     
-                                         $modelPlanes->save();
+		if (Yii::app()->authRBAC->checkAccess('planes_carga')) :
+			try {
 
-                                     endwhile;
-                                     fclose($handle);
-                                     $transaction->commit();
-                                     Yii::app()->user->setFlash('app','Se ha importado ');                                
-                              else:
+				$model = new Planes;
 
-                                Yii::app()->user->setFlash('app','Solo debe importar archivos con la extesion CSV');  
+				if (isset($_FILES['Planes'])) {
 
-                               endif;
+					$model->attributes = $_POST['Planes'];
+					$transaction = $model->dbConnection->beginTransaction();
 
-                      }
-                
-                } catch (Exception $e) {
-                      $transaction->rollBack();
-                      Yii::app()->user->setFlash('app','Error en la carga masiva. Revice el archivo .csv');  
-                } 
-         
-            $this->render('carga',array(
-			'model'=>$model,
-		));
-        endif;    
-        }
+					$fname = $_FILES['Planes']['name'];
+					$chk_ext = explode(".", $fname['file']);
+					if (strtolower(end($chk_ext)) == "csv") :
+						$filename = $_FILES['Planes']['tmp_name'];
+						$handle = fopen($filename['file'], "r");
+						while ($data = fgetcsv($handle, 500, ";")) :
+							$modelPlanes = new Planes;
+							$modelPlanes->nombre = $data[0];
+							$modelPlanes->valor = $data[1];
+							$modelPlanes->operador_id = $_POST['Planes']['operador_id'];
+							$modelPlanes->fecha_vigencia = $_POST['Planes']['fecha_vigencia'];
+							$modelPlanes->fecha_vencimiento = $_POST['Planes']['fecha_vencimiento'];
+							$modelPlanes->save();
 
-        /**
+						endwhile;
+						fclose($handle);
+						$transaction->commit();
+						Yii::app()->user->setFlash('app', 'Se ha importado ');
+					else :
+
+						Yii::app()->user->setFlash('app', 'Solo debe importar archivos con la extesion CSV');
+
+					endif;
+				}
+			} catch (Exception $e) {
+				$transaction->rollBack();
+				Yii::app()->user->setFlash('app', 'Error en la carga masiva. Revice el archivo .csv');
+			}
+
+			$this->render('carga', array(
+				'model' => $model,
+			));
+		endif;
+	}
+
+	/**
 	 * Creates a new model.
 	 * If creation is successful, the browser will be redirected to the 'view' page.
 	 */
 	public function actionCreate()
 	{
-            if (Yii::app()->authRBAC->checkAccess('planes_crear')):   
-		$model=new Planes;
+		if (Yii::app()->authRBAC->checkAccess('planes_crear')) :
+			$model = new Planes;
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+			// Uncomment the following line if AJAX validation is needed
+			// $this->performAjaxValidation($model);
 
-		if(isset($_POST['Planes']))
-		{
-			$model->attributes=$_POST['Planes'];
-                        
-                        if($model->save())                              
-                               $this->redirect(array('view','id'=>$model->id));                        	
-		}
+			if (isset($_POST['Planes'])) {
+				$model->attributes = $_POST['Planes'];
 
-		$this->render('create',array(
-			'model'=>$model,
-		));
-            endif;    
+				if ($model->save())
+					$this->redirect(array('view', 'id' => $model->id));
+			}
+
+			$this->render('create', array(
+				'model' => $model,
+			));
+		endif;
 	}
-        
-        public function operadorAID($data){
-            $valor = '';
-            
-            switch (strtolower($data)):
-                case 'etb':         $valor = '54'; break;
-                case 'mov m':       $valor = '3'; break;
-                case 'movistar m':  $valor = '3'; break;
-                case 'claro':       $valor = '6'; break;
-                case 'claro m':     $valor = '2'; break;
-                case 'claro f':     $valor = '6'; break;
-                case 'ava i':       $valor = '4'; break;
-                case 'avantel':     $valor = '4'; break;
-                case 'etb m c':     $valor = '1'; break;
-                case 'etb f n':     $valor = '5'; break;
-                case 'etb m':       $valor = '1'; break;
-                case 'dtv':         $valor = '8'; break; 
-                default:            $valor = '56';
-            endswitch;
-            
-            return $valor;
-            
-        }
 
-        /**
+	public function operadorAID($data)
+	{
+		$valor = '';
+
+		switch (strtolower($data)):
+			case 'etb':
+				$valor = '54';
+				break;
+			case 'mov m':
+				$valor = '3';
+				break;
+			case 'movistar m':
+				$valor = '3';
+				break;
+			case 'claro':
+				$valor = '6';
+				break;
+			case 'claro m':
+				$valor = '2';
+				break;
+			case 'claro f':
+				$valor = '6';
+				break;
+			case 'ava i':
+				$valor = '4';
+				break;
+			case 'avantel':
+				$valor = '4';
+				break;
+			case 'etb m c':
+				$valor = '1';
+				break;
+			case 'etb f n':
+				$valor = '5';
+				break;
+			case 'etb m':
+				$valor = '1';
+				break;
+			case 'dtv':
+				$valor = '8';
+				break;
+			default:
+				$valor = '56';
+		endswitch;
+
+		return $valor;
+	}
+
+	/**
 	 * Updates a particular model.
 	 * If update is successful, the browser will be redirected to the 'view' page.
 	 * @param integer $id the ID of the model to be updated
 	 */
 	public function actionUpdate($id)
 	{
-            if (Yii::app()->authRBAC->checkAccess('planes_editar')):   
-		$model=$this->loadModel($id);
+		if (Yii::app()->authRBAC->checkAccess('planes_editar')) :
+			$model = $this->loadModel($id);
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+			// Uncomment the following line if AJAX validation is needed
+			// $this->performAjaxValidation($model);
 
-		if(isset($_POST['Planes']))
-		{
-			$model->attributes=$_POST['Planes'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
+			if (isset($_POST['Planes'])) {
+				$model->attributes = $_POST['Planes'];
+				if ($model->save())
+					$this->redirect(array('view', 'id' => $model->id));
+			}
 
-		$this->render('update',array(
-			'model'=>$model,
-		));
-            endif;    
+			$this->render('update', array(
+				'model' => $model,
+			));
+		endif;
 	}
 
 	/**
@@ -187,13 +213,13 @@ class PlanesController extends Controller
 	 */
 	public function actionDelete($id)
 	{
-             if (Yii::app()->authRBAC->checkAccess('planes_eliminar')):   
-		$this->loadModel($id)->delete();
+		if (Yii::app()->authRBAC->checkAccess('planes_eliminar')) :
+			$this->loadModel($id)->delete();
 
-		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-            endif;    
+			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+			if (!isset($_GET['ajax']))
+				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+		endif;
 	}
 
 	/**
@@ -201,9 +227,9 @@ class PlanesController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$dataProvider=new CActiveDataProvider('Planes');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
+		$dataProvider = new CActiveDataProvider('Planes');
+		$this->render('index', array(
+			'dataProvider' => $dataProvider,
 		));
 	}
 
@@ -212,16 +238,16 @@ class PlanesController extends Controller
 	 */
 	public function actionAdmin()
 	{
-            if (Yii::app()->authRBAC->checkAccess('planes_admin')):   
-		$model=new Planes('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Planes']))
-			$model->attributes=$_GET['Planes'];
+		if (Yii::app()->authRBAC->checkAccess('planes_admin')) :
+			$model = new Planes('search');
+			$model->unsetAttributes();  // clear any default values
+			if (isset($_GET['Planes']))
+				$model->attributes = $_GET['Planes'];
 
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-            endif;    
+			$this->render('admin', array(
+				'model' => $model,
+			));
+		endif;
 	}
 
 	/**
@@ -233,9 +259,9 @@ class PlanesController extends Controller
 	 */
 	public function loadModel($id)
 	{
-		$model=Planes::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
+		$model = Planes::model()->findByPk($id);
+		if ($model === null)
+			throw new CHttpException(404, 'The requested page does not exist.');
 		return $model;
 	}
 
@@ -245,23 +271,23 @@ class PlanesController extends Controller
 	 */
 	protected function performAjaxValidation($model)
 	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='planes-form')
-		{
+		if (isset($_POST['ajax']) && $_POST['ajax'] === 'planes-form') {
 			echo CActiveForm::validate($model);
 			Yii::app()->end();
 		}
 	}
-        
-         public function cantidad(){    
-           $filename = $_FILES['Planes']['tmp_name'];
-           $archivo = fopen($filename['file'], "r");
-            $num_lineas = 0;
-            while (!feof ($archivo)) {                
-                if ($linea = fgets($archivo)){                  
-                   $num_lineas++;
-                }
-            }
-            fclose ($archivo);           
-            return $num_lineas;
-        }
+
+	public function cantidad()
+	{
+		$filename = $_FILES['Planes']['tmp_name'];
+		$archivo = fopen($filename['file'], "r");
+		$num_lineas = 0;
+		while (!feof($archivo)) {
+			if ($linea = fgets($archivo)) {
+				$num_lineas++;
+			}
+		}
+		fclose($archivo);
+		return $num_lineas;
+	}
 }
